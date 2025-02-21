889059e4b1392fe4253d90c2045b8d7bc19a43eb
ShifatRahman420 <copyremc4@gmail.com>
2025-02-21 10:23:49 +0600

    add env file

--- src/authprovider/firebaseConfig.js
+++ src/authprovider/firebaseConfig.js
@@ -1,17 +1,14 @@
-
-// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4OnDRo6vLJCZ_8anwMeSsJPPfhz2kbSI",
-  authDomain: "garir-hat.firebaseapp.com",
-  projectId: "garir-hat",
-  storageBucket: "garir-hat.firebasestorage.app",
-  messagingSenderId: "665859475923",
-  appId: "1:665859475923:web:f8a946f083695d3732c726",
-  measurementId: "G-TLPLSQ1QHQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export default app;
-
